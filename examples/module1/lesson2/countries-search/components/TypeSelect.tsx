3bd5212eebe1ce614508b102b4e35bbdabd63b97
Rafał H <rafal.hes@yahoo.com>
2024-06-28 20:45:59 +0200

    feat: add TypeSelect

---
+++ examples/module1/lesson2/countries-search/components/TypeSelect.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface TypeSelectProps {
+  queryType: string;
+  setQueryType: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const GenderSelect: React.FC<TypeSelectProps> = ({
+  queryType,
+  setQueryType,
+}) => {
+  return (
+    <label className="flex flex-col">
+      Query Type
+      <select
+        value={queryType}
+        onChange={(e) => setQueryType(e.target.value)}
+        className="border h-7 mt-1"
+      >
+        <option value="country">Country</option>
+        <option value="currency">Currency</option>
+        <option value="language">language</option>
+        <option value="capitol">capitol</option>
+      </select>
+    </label>
+  );
+};
+
+export default GenderSelect;
