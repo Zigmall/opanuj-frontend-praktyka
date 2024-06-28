5643dea39d4c67810c186e99053e585c14764e68
Rafał H <rafal.hes@yahoo.com>
2024-06-28 20:41:15 +0200

    feat: add project files and QueryField component

---
+++ examples/module1/lesson2/countries-search/components/QueryField.tsx
@@ -0,0 +1,22 @@
+const QueryField = ({
+  query,
+  setQuery,
+}: {
+  query: string;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  return (
+    <label className="flex flex-col">
+      Query
+      <input
+        className="border h-7 mt-1 indent-2"
+        type="text"
+        placeholder="Rick Sanchez..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </label>
+  );
+};
+
+export default QueryField;
