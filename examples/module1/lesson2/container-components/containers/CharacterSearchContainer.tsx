950c54e5e622e1f6595e8f07aea0fcea3ecf76f6
Rafał H <rafal.hes@yahoo.com>
2024-06-28 13:43:18 +0200

    feat: create custom hook for searching characters

--- examples/module1/lesson2/container-components/containers/CharacterSearchContainer.tsx
+++ examples/module1/lesson2/container-components/containers/CharacterSearchContainer.tsx
@@ -1,27 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import CharacterList from '../components/CharacterList';
 import SearchTitle from '../components/SearchTitle';
-import { Character } from '../types/Character';
 import GenderField from '../components/GenderField';
 import NameField from '../components/NameField';
 import SortBy from '../components/SortBy';
+import useCharacterSearch from '../hooks/useCharacterSearch';
 
 function CharacterSearchContainer() {
   const [name, setName] = useState('');
   const [gender, setGender] = useState('');
-  const [characters, setCharacters] = useState<Character[]>([]);
+  const characters = useCharacterSearch(name, gender);
   const [sortOption, setSortOption] = useState('');
 
-  useEffect(() => {
-    if (name || gender) {
-      fetch(
-        `https://rickandmortyapi.com/api/character/?name=${name}&gender=${gender}`
-      )
-        .then((response) => response.json())
-        .then((data) => setCharacters(data.results || []))
-        .catch((error) => console.error('Error fetching data:', error));
-    }
-  }, [name, gender]);
 
   const sortedCharacters = [...characters].sort((a, b) => {
     if (sortOption === 'name') {
