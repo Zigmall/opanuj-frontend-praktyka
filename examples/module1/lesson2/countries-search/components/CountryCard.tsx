cc2d002171b8a13f909043810ba0a7e8537aa644
Rafał H <rafal.hes@yahoo.com>
2024-06-29 13:52:06 +0200

    feat: add condition display on CountryCard

--- examples/module1/lesson2/countries-search/components/CountryCard.tsx
+++ examples/module1/lesson2/countries-search/components/CountryCard.tsx
@@ -1,10 +1,10 @@
 import { Country } from '../types/Country';
 
 export function CountryCard({ country }: { country: Country }) {
-  return (
+  return country.flags.svg && (
     <>
       <h3>{country.name.common}</h3>
-      <img src={country.flag.png} alt={country.name.common} />
+      <img src={country.flags.svg} alt={country.name.common} />
     </>
   );
 }
