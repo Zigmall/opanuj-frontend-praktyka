e6eccc0f538cfc1ca0e514690d04f5ddf30e48c3
Rafał H <rafal.hes@yahoo.com>
2024-06-29 13:53:45 +0200

    feat: add useCountrySearch

--- examples/module1/lesson2/countries-search/components/CountryList.tsx
+++ examples/module1/lesson2/countries-search/components/CountryList.tsx
@@ -1,7 +1,7 @@
 import { Country } from '../types/Country';
 import { CountryCard } from './CountryCard';
 
-export function CharacterList({ countries }: { countries: Country[] }) {
+export function CountryList({ countries }: { countries: Country[] }) {
   return (
     <ol className="grid grid-cols-1 gap-4 list-none md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {countries.map((country, index) => (
