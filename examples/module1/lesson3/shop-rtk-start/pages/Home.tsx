7654f30e2f0c7d6aed854dda02e9ddcee98f4f16
Rafał H <rafal.hes@yahoo.com>
2024-07-02 15:23:43 +0200

    refactor: replace productContext

--- examples/module1/lesson3/shop-rtk-start/pages/Home.tsx
+++ examples/module1/lesson3/shop-rtk-start/pages/Home.tsx
@@ -1,24 +1,31 @@
-import { useContext } from 'react';
 import Product from '../components/Product';
-import { ProductContext } from '../contexts/ProductContext';
+import { useGetProductsQuery } from '../services/product';
 
 const Home = () => {
-  const { products } = useContext(ProductContext);
+  const { data, error, isLoading } = useGetProductsQuery();
 
   return (
     <div>
-      <section className="py-20">
-        <div className="container mx-auto">
-          <h1 className="text-3xl font-semibold mb-10 text-center">
-            Explore Our Products
-          </h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 lg:mx-8 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
-            {products.map((product) => {
-              return <Product product={product} key={product.id} />;
-            })}
+      {error ? (
+        <div>Error fetching products</div>
+      ) : isLoading ? (
+        <div>Loading...</div>
+      ) : data ? (
+        <section className="py-20">
+          <div className="container mx-auto">
+            <h1 className="text-3xl font-semibold mb-10 text-center">
+              Explore Our Products
+            </h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 lg:mx-8 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
+              {Array.isArray(data) ? (
+                data.map((product) => {
+                  return <Product product={product} key={product.id} />;
+                })
+              ) : null}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      ) : null}
     </div>
   );
 };
