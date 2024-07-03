e2909a605b77cb17ab08f0aafeeea9b15f875229
Rafał H <rafal.hes@yahoo.com>
2024-07-03 18:28:15 +0200

    refactor: import useGetUsersClient hook

--- examples/module1/lesson4/abort-feedback/App.tsx
+++ examples/module1/lesson4/abort-feedback/App.tsx
@@ -1,35 +1,27 @@
-import React, { useEffect, useState } from 'react';
-
-interface User {
-  id: number;
-  name: string;
-}
-
-const API_URL = '/api/data/users?timeout=10000';
+import { useGetUsersClient } from './useGetUsersClient';
 
 const App = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const { users, isDelayed, retryToGetUsers } = useGetUsersClient();
 
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then(({ users }) => {
-        setUsers(users);
-      });
-  }, []);
+  
 
   return (
     <div>
       <div className="flex flex-row items-center justify-between py-4">
         <h1 className="text-2xl font-bold">Users</h1>
-        <div className="flex flex-row items-center">
-          <p className="mr-2">
-            Sorry, there seems to be connectivity issues...
-          </p>
-          <button className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4">
-            Try again
-          </button>
-        </div>
+        {isDelayed && (
+          <div className="flex flex-row items-center">
+            <p className="mr-2">
+              Sorry, there seems to be connectivity issues...
+            </p>
+            <button
+              onClick={retryToGetUsers}
+              className="text-blue-400 bg-blue-200 hover:text-blue-200 hover:bg-blue-400 rounded-md p-4"
+            >
+              Try again
+            </button>
+          </div>
+        )}
       </div>
       <ul className="space-y-2">
         {users.map((user, index) => (
